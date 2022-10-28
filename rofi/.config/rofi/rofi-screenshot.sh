25e8c598787e136f4fce095cfe95f27c524435f6
Douglas Hoffmann <douglashoffmann@outlook.es>
2022-10-27 23:30:09 -0300

    feat: rofi screenshots

---
+++ rofi/.config/rofi/rofi-screenshot.sh
@@ -0,0 +1,27 @@
+(rofi -dmenu | (read sel;
+# OUTDIR=$HOME/Pictures/screenshot/
+MAIM=maim
+# SCREENSHOT=$OUTDIR/$(date +%F-%T).png
+case "${sel%.*}" in
+	1)
+		# whole screen capture
+		$MAIM | xclip -selection clipboard -t image/png
+		;;
+	2)
+		# select region to capture
+		$MAIM -s | xclip -selection clipboard -t image/png
+		;;
+	3)
+		# capture active window
+		$MAIM -i $(xdotool getactivewindow) | xclip -selection clipboard -t image/png
+		;;
+	*)
+		return
+		;;
+	esac
+	echo $SCREENSHOT
+)) <<end
+1. Whole screen
+2. Select region
+3. Active window
+end
